ed95c995539c963d91490f93f4cd5f022139525f
Julzzyboi <152506131+Julzzyboi@users.noreply.github.com>
2025-05-11 00:20:09 +0800

    nc

--- adminDashboard.php
+++ adminDashboard.php
@@ -63,7 +63,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    </head>
+</head>
     
     <body>
         <label>
@@ -183,6 +183,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           <section id="inventorySection" class="Page-Section">
 
             <div class="ProductForm-Container">
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Add Product</button>
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      Hotdog
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             <table class="table">
                 <thead>
                   <tr>
@@ -197,21 +219,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <tbody>
                   <tr>
                     <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    
                   </tr>
                   <tr>
                     <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                   
                   </tr>
                   <tr>
                     <th scope="row">3</th>
-                    <td>John</td>
-                    <td>Doe</td>
-                    <td>@social</td>
+                   
                   </tr>
                 </tbody>
               </table>
@@ -219,6 +235,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
           </section>
 
+          <!-- Account Section -->
           <section id="accountSection" class="Page-Section">
             <div class="Account-Container">
               <form id="AdminForm" method="POST" action="adminDashboard.php">
