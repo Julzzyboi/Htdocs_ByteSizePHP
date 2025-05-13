eb381cf301f3df638d14fdf7cb097361e7def2a3
Julzzyboi <152506131+Julzzyboi@users.noreply.github.com>
2025-05-13 23:31:50 +0800

    sw

--- Product.js
+++ Product.js
@@ -1,15 +1,15 @@
-sdocument.querySelectorAll('.secondary-nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        if (targetSection) {
-            const offset = -80; // Adjust this value to set the distance from the top
-            const targetPosition = targetSection.offsetTop - offset;
-            window.scrollTo({
-                top: targetPosition,
-                behavior: 'smooth'
-            });
-        }
-    });
-})
+// sdocument.querySelectorAll('.secondary-nav a').forEach(anchor => {
+//     anchor.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         const targetId = this.getAttribute('href').substring(1);
+//         const targetSection = document.getElementById(targetId);
+//         if (targetSection) {
+//             const offset = -80; // Adjust this value to set the distance from the top
+//             const targetPosition = targetSection.offsetTop - offset;
+//             window.scrollTo({
+//                 top: targetPosition,
+//                 behavior: 'smooth'
+//             });
+//         }
+//     });
+// })
